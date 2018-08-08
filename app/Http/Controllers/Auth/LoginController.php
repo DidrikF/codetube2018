61eb7f6169457c73c6c9fe5c6f2b955ecbe8f73b
DidrikF <didrik.fleischer@gmail.com>
2018-08-08 15:56:34 +0200

    php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -20,11 +20,7 @@ class LoginController extends Controller
     */
     
     use AuthenticatesUsers;
-    /*
-    use AuthenticatesUsers {
-        logout as performLogout;
-    }
-    */
+
 
     /**
      * Where to redirect users after login.
@@ -42,38 +38,5 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
-
-    
-        /**
-     * Log the user out of the application.
-     *
-     * @param \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function logout(Request $request)
-    {
-        $this->guard()->logout();
-
-        $request->session()->flush();
-
-        $request->session()->regenerate();
-
-        return redirect('/livedemo/codetube');
-    }
-
-    /*
-    public function logout(Request $request)
-    {
-        $this->performLogout($request);
-        return redirect()->route('/');
-    }
-    public function logout(Request $request) {
-        $this->guard()->logout();
-
-        $request->session()->invalidate();
-
-        return redirect(config('APP_URL'));
-    }
-    */
     
 }
