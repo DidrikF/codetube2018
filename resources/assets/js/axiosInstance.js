acf084311d83d8ae267d3c2bc0f33811939d7625
DidrikF <didrik.fleischer@gmail.com>
2018-08-08 17:15:18 +0200

    php

--- resources/assets/js/axiosInstance.js
+++ resources/assets/js/axiosInstance.js
@@ -3,6 +3,7 @@ const instance = axios.create({
     baseURL: 'http://didrikfleischer.com/livedemo/codetube',
     headers: {
         'X-CSRF-TOKEN': window.Laravel.csrfToken,
+        'X-Requested-With': 'XMLHttpRequest',
     }
 })
 
